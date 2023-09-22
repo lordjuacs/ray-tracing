8299d5eadbac6c724e43c77d8c0230d27b90be80
lordjuacs <juacsr@gmail.com>
2023-09-22 13:15:46 -0500

    Add light spheres (multiple)

--- main.cpp
+++ main.cpp
@@ -23,40 +23,50 @@ void basica() {
     p1->init_constants(1, 0);
     p1->ke = 1;
     objetos.emplace_back(p1);
-    p1 = new Esfera(vec3(10, 0, 20), 8, vec3(0.1, 0.1, 0.1));
-    p1->init_constants(0.8, 0.2);
-    p1->ke = 0.9;
-    p1->transparency = true;
-    p1->ior = 1.5;
+
+    p1 = new Esfera(vec3(20, 10, 20), 4, vec3(1,1,0));
+    p1->init_constants(0.8, 0.2, 32);
+    p1->light = true;
+    //p1->ke = 0.9;
+    //p1->transparency = true;
+    //p1->ior = 1.5;
+
     objetos.emplace_back(p1);
-    p1 = new Esfera(vec3(0, 10, 0), 8, vec3(1, 0, 0));
+    vector<Luz *> luces;
+    Luz* pluz = new Luz( vec3(20, 10, 20), vec3(1,1,0));
+    luces.emplace_back(pluz);
+
+    p1 = new Esfera(vec3(0, 10, 0), 3, vec3(1, 1, 0));
     p1->init_constants(0.6, 0.4, 32);
-    p1->ke = 0.8;
-    p1->transparency = false;
-    p1->ior = 1.2;
+    //p1->ke = 0.8;
+    //p1->transparency = false;
+    //p1->ior = 1.2;
+    p1->light = true;
     objetos.emplace_back(p1);
+    pluz =new Luz (vec3(0, 10, 0), vec3(1, 1, 0));
+    luces.emplace_back(pluz);
 
     p1 = new Plano(vec3(0, 1, 0), 1, vec3(0.123, 0.456, 0.789));
     p1->init_constants(0.9, 0.1);
     p1->ke = 0.1;
     objetos.emplace_back(p1);
 
-    p1 = new Cilindro(vec3(-20, 0, 0), vec3(-10, 10, 0), 5, vec3(0, 1, 1));
+    p1 = new Cilindro(vec3(0,0,0), vec3(0,25,0), 7, vec3(1, 1, 1));
     p1->init_constants(0.7, 0.3, 8);
+    p1->transparency = true;
+//p1->ke  = 0.8;
     objetos.emplace_back(p1);
 
 
-    vector<Luz *> luces;
-    Luz luz(vec3(30, 30, 30), vec3(1, 1, 1));
-    luces.emplace_back(&luz);
+
 
     Camara cam;
-    for (int x = 0, n = 1; x < 5; x++, n++) {
-        cam.configurar(3, 60, 600, 800,
-                       vec3(x, 5, 50),
+    for (int x = 0, n = 1; x <50; x+=3, n++) {
+        cam.configurar(3, 100, 600, 800,
+                       vec3(x, 5+x, 50),
                        vec3(0, 0, 0),
                        vec3(0, 1, 0));
-        //cam.renderizar(objetos, luces[0], n);
+        cam.renderizar(objetos, luces, n);
     }
 }
 
@@ -162,9 +172,9 @@ void escena4() {
     plano.init_constants(0.3, 0.1, 8, 0.4);
     vector<Objeto *> objetos;
     objetos.emplace_back(&esf);
-    objetos.emplace_back(&esf2);
-    objetos.emplace_back(&esf3);
-    objetos.emplace_back(&esfLuz);
+    //objetos.emplace_back(&esf2);
+    //objetos.emplace_back(&esf3);
+    //objetos.emplace_back(&esfLuz);
     objetos.emplace_back(&plano);
     Camara cam;
     cam.configurar(3, 60, 800, 600,
@@ -175,9 +185,9 @@ void escena4() {
 }
 
 int main() {
-    escena4();
+    //escena4();
     //escena3();
     //escena2();
-    //basica();
+    basica();
     return 0;
 }
